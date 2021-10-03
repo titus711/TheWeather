890440e669c5d6af3ecc59063e9368c38907c238
Tito Nangi <titusnangi@gmail.com>
2021-10-03 16:41:25 +0200

    creating the Constants and TimeAndDateConverter classes

---
+++ app/src/main/java/com/titusnangi/theweather/utils/Constants.java
@@ -0,0 +1,25 @@
+package com.titusnangi.theweather.utils;
+
+public class Constants {
+
+
+    /** Constants values are here */
+
+
+    public static class baseUrl{
+        public final static String WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/";
+        public final static String WEATHER_IMAGE_BASE_URL = "https://openweathermap.org/img/w/";
+    }
+
+    public static class apiKeys{
+        public final static String WEATHER_API = "3714269f3192f8badb5b588c3e5a14af";
+    }
+
+    public static class defaultLatLng{
+        public final static double DEFAULT_LAT = 28.2184;
+        public final static double DEFAULT_LNG = -25.7313;
+
+    }
+
+
+}
